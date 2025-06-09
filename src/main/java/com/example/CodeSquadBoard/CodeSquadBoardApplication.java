75166c1dbd68da8254c4edd0a4fac253af6b59d3
jkchoi490 <jkchoi490@daum.net>
2025-06-09 11:04:10 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/CodeSquadBoard/CodeSquadBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeSquadBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSquadBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSquadBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeSquadBoard/CodeSquadBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeSquadBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSquadBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
